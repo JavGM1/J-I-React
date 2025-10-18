7995dfb5ba2556d38589fe69157632c5b9d63089
JavGM1 <ja.galarce@duocuc.cl>
2025-10-18 01:20:49 -0300

    Versión inicial con Navbar de prueba

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,21 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import AppNavbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      {/* Navbar en la parte superior */}
+      <AppNavbar />
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
+          <img src={reactLogo} className="logo" alt="React logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://media1.tenor.com/m/1iSARWJr-TEAAAAC/among-us-twerk.gif" target="_blank" rel="noopener noreferrer">
+          <img src="https://media1.tenor.com/m/1iSARWJr-TEAAAAC/among-us-twerk.gif" className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
